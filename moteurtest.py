1cbb26bbb42084b486af8ec5aac44ef6645c408c
julienGautier77 <julien.gautier@ensta.fr>
2021-09-22 16:52:47 +0200

    correct bug

--- moteurtest.py
+++ moteurtest.py
@@ -1,7 +1,6 @@
 
 from PyQt5.QtCore import QSettings
 
-confTest=QSettings('./fichiersConfig/configMoteurTest.ini', QSettings.IniFormat)
 
 class MOTORTEST():
     
@@ -9,26 +8,29 @@ class MOTORTEST():
         
         super(MOTORTEST, self).__init__()
         self.moteurname=mot1
+        self.confTest=QSettings('./fichiersConfig/configMoteurTest.ini', QSettings.IniFormat)
+
         #print(self.moteurname)
-        self.numMoteur=int(confTest.value(self.moteurname+'/numMoteur'))
+        self.numMoteur=int(self.confTest.value(self.moteurname+'/numMoteur'))
         #print('init motor test')
- 
+        
     def rmove(self,pas,vitesse=1000):
         
-        print('motor',self.moteurname,'rmove',pas)
-        #return recu
+        print('motor : ',self.moteurname,'rmove of ',pas)
+        posi= float(self.confTest.value(self.moteurname+'/Pos'))+pas
+        self.confTest.setValue(self.moteurname+'/Pos',posi)
 
     def move(self,position,vitesse=1000):
-        print('motor',self.moteurname,'move',position)
-        
+        print('motor : ',self.moteurname,'move to ',position)
+        self.confTest.setValue(self.moteurname+'/Pos',position)
     
     def position(self):
-        return 12345
+        return float( self.confTest.value(self.moteurname+'/Pos'))
     
     def setzero(self):
-        confTest.setValue(self.moteurname+"/Pos",0)
+        self.confTest.setValue(self.moteurname+"/Pos",0)
     
     def name(self):
         return self.moteurname
     def stopMotor(self):
-        print('stop motor')
+        print('Stop motor')
